cea6fe91dd42776e4acfd896ff6269ed8e9353d2
meredith <dmzart921@gmail.com>
2025-08-01 17:09:23 +0200

    Initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
+"use client";
+
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+        <Button onClick={() => alert("Clicked")}>Click me</Button>
         <Image
           className="dark:invert"
           src="/next.svg"
